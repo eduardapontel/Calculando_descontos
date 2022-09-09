7508a8b07e9df507ea88a0e2f20a72ddd8875b5f
Eduarda Alves Pontel <109531518+eduardapontel@users.noreply.github.com>
2022-09-09 08:11:30 -0300

    Add files via upload

--- Exercicio_012.py
+++ Exercicio_012.py
@@ -4,7 +4,21 @@ print('{:^35}'.format('CALCULANDO DESCONTOS'))
 print(bordas)
 print()
 
-pp = float(input('Digite o preço do produto? R$'))
-desconto = int(input('Digite a porcentagem de desconto: '))
+pp = ''
+
+while type(pp) != float:
+    try:
+        pp = float(input('Digite o preço do produto? R$'))
+    except:
+        print('Por favor digite um preço válido!')
+
+desconto = ''
+
+while type(desconto) != float:
+    try:
+        desconto = float(input('Digite a porcentagem de desconto: '))
+    except:
+        print('Por favor digite um desconto válido!')
+
 print(f'Com a promoção de {desconto}% de desconto o desconto será de R${pp/desconto:.2f}'
       f' e o produto que antes custava R${pp:.2f} vai passar a custar R${pp-pp*(desconto/100):.2f}.')
